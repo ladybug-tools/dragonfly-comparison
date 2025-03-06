c880edd2b9340b69d96a70f51e602f5c9b49f900
Chris Mackey <chris@ladybug.tools>
2025-03-05 17:28:38 -0800

    feat(init): Add all methods needed for Room2D comparison

--- dragonfly_comparison/properties/model.py
+++ dragonfly_comparison/properties/model.py
@@ -22,6 +22,32 @@ class ModelComparisonProperties(object):
         """Get the Model object hosting these properties."""
         return self._host
 
+    def set_from_model(self, comparison_model, reset_unmatched=True):
+        """Set the attributes of Room2DComparisonProperties using another Model.
+
+        Args:
+            comparison_model: A dragonfly Model to which the host Model is
+                being compared. Room2Ds in the host model with identifiers
+                matching the comparison_model model will have their comparison
+                properties set using them.
+            reset_unmatched: A boolean to note whether rooms in the host model
+                should have their comparison room properties reset if they are not
+                matched with any room in the comparison_model. (Default: True).
+        """
+        for base_room in self.host.room_2ds:
+            for comp_room in comparison_model.room_2ds:
+                if base_room.identifier == comp_room.identifier:
+                    base_room.properties.comparison.set_from_room_2d(comp_room)
+                    break
+            else:
+                if reset_unmatched:
+                    base_room.properties.comparison.reset()
+
+    def reset(self):
+        """Reset the comparison attributes using the host Model."""
+        for base_room in self.host.room_2ds:
+            base_room.properties.comparison.reset()
+
     def apply_properties_from_dict(self, data):
         """Apply the comparison properties of a dictionary to the host Model of this object.
 
@@ -42,8 +68,7 @@ class ModelComparisonProperties(object):
 
     def to_dict(self):
         """Return Model comparison properties as a dictionary."""
-        base = {'comparison': {'type': 'ModelComparisonProperties'}}
-        return base
+        return {'comparison': {'type': 'ModelComparisonProperties'}}
 
     def duplicate(self, new_host=None):
         """Get a copy of this Model.
--- tests/room2d_extend_test.py
+++ tests/room2d_extend_test.py
@@ -1,12 +1,13 @@
 """Tests the features that dragonfly_comparison adds to dragonfly_core Room2D."""
-from ladybug_geometry.geometry3d.pointvector import Point3D
-from ladybug_geometry.geometry3d.face import Face3D
+import pytest
 
+from ladybug_geometry.geometry2d import Point2D
+from ladybug_geometry.geometry3d import Point3D, Face3D
 from honeybee.boundarycondition import boundary_conditions as bcs
-
 from dragonfly.room2d import Room2D
 from dragonfly.windowparameter import SimpleWindowRatio
 from dragonfly.shadingparameter import Overhang
+from dragonfly.skylightparameter import GriddedSkylightRatio
 
 from dragonfly_comparison.properties.room2d import Room2DComparisonProperties
 
@@ -25,24 +26,60 @@ def test_comparison_properties():
     assert isinstance(room.properties.comparison, Room2DComparisonProperties)
 
 
-def test_default_properties():
+def test_comparison_properties_setability():
     """Test the auto-assigning of Room2D properties."""
     pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
     ashrae_base = SimpleWindowRatio(0.4)
     room = Room2D('SquareShoebox', Face3D(pts), 3)
+    room.is_top_exposed = True
     room.set_outdoor_window_parameters(ashrae_base)
+    room.skylight_parameters = GriddedSkylightRatio(0.05)
 
-    assert room.properties.comparison.modifier_set.identifier == \
-        'Generic_Interior_Visible_Modifier_Set'
+    assert room.properties.comparison.comparison_floor_geometry is None
+    assert room.properties.comparison.comparison_windows is None
+    assert room.properties.comparison.comparison_skylight is None
+    assert room.properties.comparison.floor_area_difference == 0
+    assert room.properties.comparison.wall_sub_face_area_difference == 0
+    assert room.properties.comparison.roof_sub_face_area_difference == 0
+
+    room.properties.comparison.reset()
+    assert isinstance(room.properties.comparison.comparison_floor_geometry, Face3D)
+    assert isinstance(room.properties.comparison.comparison_windows, tuple)
+    assert isinstance(room.properties.comparison.comparison_skylight, GriddedSkylightRatio)
+    assert room.properties.comparison.floor_area_difference == 0
+    assert room.properties.comparison.wall_sub_face_area_difference == 0
+    assert room.properties.comparison.roof_sub_face_area_difference == 0
+
+    snap_points = (Point2D(10.5, 0), Point2D(10.5, 10.5))
+    room.snap_to_points(snap_points, 1.0)
+    assert room.properties.comparison.floor_area_difference == \
+        pytest.approx(7.625, abs=1e-3)
+    assert room.properties.comparison.wall_sub_face_area_difference == \
+        pytest.approx(1.8142776, abs=1e-3)
+    assert room.properties.comparison.roof_sub_face_area_difference == \
+        pytest.approx(0.38125, abs=1e-3)
+    assert room.properties.comparison.sub_face_area_difference == \
+        pytest.approx(2.1955276, abs=1e-3)
+
+    assert room.properties.comparison.floor_area_percent_change == \
+        pytest.approx(7.625, abs=1e-3)
+    assert room.properties.comparison.wall_sub_face_area_percent_change == \
+        pytest.approx(3.779745, abs=1e-3)
+    assert room.properties.comparison.roof_sub_face_area_percent_change == \
+        pytest.approx(7.625, abs=1e-3)
+    assert room.properties.comparison.sub_face_area_percent_change == \
+        pytest.approx(4.1425, abs=1e-3)
 
 
 def test_duplicate():
     """Test what happens to comparison properties when duplicating a Room2D."""
-    default_set = ModifierSet('Tinted_Window_Set')
     pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
     ashrae_base = SimpleWindowRatio(0.4)
     room_original = Room2D('SquareShoebox', Face3D(pts), 3)
+    room_original.is_top_exposed = True
     room_original.set_outdoor_window_parameters(ashrae_base)
+    room_original.skylight_parameters = GriddedSkylightRatio(0.05)
+    room_original.properties.comparison.reset()
     room_dup_1 = room_original.duplicate()
 
     assert room_original.properties.comparison.host is room_original
@@ -50,45 +87,42 @@ def test_duplicate():
     assert room_original.properties.comparison.host is not \
         room_dup_1.properties.comparison.host
 
-    assert room_original.properties.comparison.modifier_set == \
-        room_dup_1.properties.comparison.modifier_set
-    room_dup_1.properties.comparison.modifier_set = default_set
-    assert room_original.properties.comparison.modifier_set != \
-        room_dup_1.properties.comparison.modifier_set
-
-    room_dup_2 = room_dup_1.duplicate()
-
-    assert room_dup_1.properties.comparison.modifier_set == \
-        room_dup_2.properties.comparison.modifier_set
-    room_dup_2.properties.comparison.modifier_set = None
-    assert room_dup_1.properties.comparison.modifier_set != \
-        room_dup_2.properties.comparison.modifier_set
+    assert room_original.properties.comparison.floor_area_difference == \
+        room_dup_1.properties.comparison.floor_area_difference
+    snap_points = (Point2D(10.5, 0), Point2D(10.5, 10.5))
+    room_dup_1.snap_to_points(snap_points, 1.0)
+    assert room_original.properties.comparison.floor_area_difference != \
+        room_dup_1.properties.comparison.floor_area_difference
 
 
 def test_to_dict():
     """Test the Room2D to_dict method with comparison properties."""
-    default_set = ModifierSet('Tinted_Window_Set')
     pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
     ashrae_base = SimpleWindowRatio(0.4)
-    overhang = Overhang(1)
-    boundarycs = (bcs.outdoors, bcs.ground, bcs.outdoors, bcs.ground)
-    window = (ashrae_base, None, ashrae_base, None)
-    shading = (overhang, None, None, None)
-    room = Room2D('ShoeBoxZone', Face3D(pts), 3, boundarycs, window, shading)
+    room = Room2D('SquareShoebox', Face3D(pts), 3)
+    room.is_top_exposed = True
+    room.set_outdoor_window_parameters(ashrae_base)
+    room.skylight_parameters = GriddedSkylightRatio(0.05)
 
     rd = room.to_dict()
     assert 'properties' in rd
     assert rd['properties']['type'] == 'Room2DProperties'
     assert 'comparison' in rd['properties']
     assert rd['properties']['comparison']['type'] == 'Room2DComparisonProperties'
-    assert 'modifier_set' not in rd['properties']['comparison'] or \
-        rd['properties']['comparison']['modifier_set'] is None
+    assert 'floor_boundary' not in rd['properties']['comparison'] or \
+        rd['properties']['comparison']['floor_boundary'] is None
+    assert 'window_parameters' not in rd['properties']['comparison'] or \
+        rd['properties']['comparison']['window_parameters'] is None
+    assert 'skylight_parameters' not in rd['properties']['comparison'] or \
+        rd['properties']['comparison']['skylight_parameters'] is None
 
-    room.properties.comparison.modifier_set = default_set
-    room.properties.comparison.grid_parameters = [RoomGridParameter(0.3)]
+    room.properties.comparison.reset()
+    snap_points = (Point2D(10.5, 0), Point2D(10.5, 10.5))
+    room.snap_to_points(snap_points, 1.0)
     rd = room.to_dict()
-    assert rd['properties']['comparison']['modifier_set'] is not None
-    assert len(rd['properties']['comparison']['grid_parameters']) == 1
+    assert rd['properties']['comparison']['floor_boundary'] is not None
+    assert len(rd['properties']['comparison']['window_parameters']) == 4
+    assert rd['properties']['comparison']['skylight_parameters'] is not None
 
 
 def test_from_dict():
@@ -96,14 +130,21 @@ def test_from_dict():
     pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
     ashrae_base = SimpleWindowRatio(0.4)
     room = Room2D('SquareShoebox', Face3D(pts), 3)
+    room.is_top_exposed = True
     room.set_outdoor_window_parameters(ashrae_base)
-
-    default_set = ModifierSet('Tinted_Window_Set')
-    room.properties.comparison.modifier_set = default_set
-    room.properties.comparison.grid_parameters = [RoomGridParameter(0.3)]
+    room.skylight_parameters = GriddedSkylightRatio(0.05)
+    room.properties.comparison.reset()
+    snap_points = (Point2D(10.5, 0), Point2D(10.5, 10.5))
+    room.snap_to_points(snap_points, 1.0)
 
     rd = room.to_dict()
     new_room = Room2D.from_dict(rd)
-    assert new_room.properties.comparison.modifier_set.identifier == 'Tinted_Window_Set'
-    assert len(new_room.properties.comparison.grid_parameters) == 1
+    assert new_room.properties.comparison.floor_area_difference == \
+        pytest.approx(7.625, abs=1e-3)
+    assert new_room.properties.comparison.wall_sub_face_area_difference == \
+        pytest.approx(1.8142776, abs=1e-3)
+    assert new_room.properties.comparison.roof_sub_face_area_difference == \
+        pytest.approx(0.38125, abs=1e-3)
+    assert new_room.properties.comparison.sub_face_area_difference == \
+        pytest.approx(2.1955276, abs=1e-3)
     assert new_room.to_dict() == rd
