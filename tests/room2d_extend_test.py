8e0b2493c92b734c245b2179b82dbb8c925b07b3
Chris Mackey <chris@ladybug.tools>
2025-03-06 13:25:54 -0800

    fix(room2d): Add a method to restore a Room2D to its comparison

--- tests/room2d_extend_test.py
+++ tests/room2d_extend_test.py
@@ -71,6 +71,33 @@ def test_comparison_properties_setability():
         pytest.approx(4.1425, abs=1e-3)
 
 
+def test_restore():
+    """Test the restoring of a Room2D to it's comparison."""
+    pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
+    ashrae_base = SimpleWindowRatio(0.4)
+    room = Room2D('SquareShoebox', Face3D(pts), 3)
+    room.is_top_exposed = True
+    room.set_outdoor_window_parameters(ashrae_base)
+    room.skylight_parameters = GriddedSkylightRatio(0.05)
+    room.properties.comparison.reset()
+    snap_points = (Point2D(10.5, 0), Point2D(10.5, 10.5))
+    room.snap_to_points(snap_points, 1.0)
+
+    assert room.properties.comparison.floor_area_difference == \
+        pytest.approx(7.625, abs=1e-3)
+    assert room.properties.comparison.wall_sub_face_area_difference == \
+        pytest.approx(1.8142776, abs=1e-3)
+    assert room.properties.comparison.roof_sub_face_area_difference == \
+        pytest.approx(0.38125, abs=1e-3)
+    assert room.properties.comparison.sub_face_area_difference == \
+        pytest.approx(2.1955276, abs=1e-3)
+
+    room = room.properties.comparison.restore()
+    assert room.properties.comparison.floor_area_difference == 0
+    assert room.properties.comparison.wall_sub_face_area_difference == 0
+    assert room.properties.comparison.roof_sub_face_area_difference == 0
+
+
 def test_duplicate():
     """Test what happens to comparison properties when duplicating a Room2D."""
     pts = (Point3D(0, 0, 3), Point3D(10, 0, 3), Point3D(10, 10, 3), Point3D(0, 10, 3))
